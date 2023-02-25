b79b0c45faa70ed521dd47265b16e537d67caf5c
Okafor Chukwunonso Joseph <chukwunonsoinfo@gmail.com>
2023-02-25 04:56:28 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints 0 to 9 in base 10
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
